731379f53302cfda3bd4f6db2b10ee2eb2548ac4
justinrlok <justinrlok@gmail.com>
2015-10-09 08:01:39 -0400

    Prototype
    Used a tutorial to understand the MEAN stack.

---
+++ PrototypeBB/README.md
@@ -0,0 +1,7 @@
+# Book Bodega Prototype
+
+REFERENCE
+Chirp Tutorial:
+      // Handles posts, login/authentication, database configuration
+      https://github.com/microsoftlearning/chirp
+      https://www.youtube.com/watch?v=Jh0er2pRcq8
---
+++ PrototypeBB/routes/index.js
@@ -0,0 +1,14 @@
+/**
+ * index.js
+ * Routing for root webpage
+ */
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    //console.log("request is " + req.user.username);
+    res.render('index', { title: "Book Bodega"});
+});
+
+module.exports = router;
