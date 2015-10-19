b5b129e0c78599bcf7be37dfd980421ee94bac20
cfee <cfee89@gmail.com>
2015-10-19 11:49:37 -0400

    Created a book schema in models.js and modified the navbar to have 3 primary buttons. Buttons are large for mobile frindliness. Brand still needs an image and no hrefs have been used.

--- PrototypeBB/models/models.js
+++ PrototypeBB/models/models.js
@@ -7,18 +7,28 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new mongoose.Schema({
-    username: String,
+ username: String,
     password: String, //hash created from password
     created_at: {type: Date, default: Date.now}
 });
 
+var bookSchema = new mongoose.Schema({
+    author: String,
+    title: String,
+    ISBN: Number,
+    dept: String,
+    course: String
+
+});
 var postSchema = new mongoose.Schema({
     //created_by: {type: Schema.ObjectId, ref: 'User'},
     created_by: String,
     created_at: {type: Date, default: Date.now},
-    text: String
+    saleType: String,
+    book: String
 });
 
 // Declare a model called User which has the Schema userSchema
 mongoose.model('User', userSchema);
 mongoose.model('Post', postSchema);
+mongoose.model('Book', bookSchema);
