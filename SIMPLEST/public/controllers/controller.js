bdff9fc9d64e9d844246a3a7abd0de3e0eddd550
cfee <cfee89@gmail.com>
2015-11-02 14:32:19 -0500

    modified index.html to assign approrpiate ng-model fields to create the desired model to be saved to the db

--- SIMPLEST/public/controllers/controller.js
+++ SIMPLEST/public/controllers/controller.js
@@ -5,11 +5,13 @@ myApp.controller('appController',['$scope','$http',
 	function($scope,$http){
 		console.log("hello world from the controller");
 //on a successful get request to /productlist, print the productlist to the console
+//changed into a function refresh that can be called to consistantly keep the list updated 
+//on the front end
 var refresh = function(){
-$http.get('/productlist').success(function(response){
+$http.get('/postlist').success(function(response){
 	console.log("I got the data requested");
-	$scope.productlist = response;
-	$scope.products = "";
+	$scope.postlist = response;
+	$scope.post = "";
 	});
 };
 
@@ -18,11 +20,11 @@ refresh();
 //defines function for the add button 
 //calls $http.post, to create a post request to /productlist
 //sending $scope.products as the body of the req
-$scope.addProduct = function(){
-	console.log($scope.products);
+$scope.createPost = function(){
+	console.log($scope.post);
 	
 	
-	$http.post('/productlist', $scope.products).success(function(response){
+	$http.post('/postlist', $scope.post).success(function(response){
 		console.log(response);
 		refresh();
 	});
--- SIMPLEST/server.js
+++ SIMPLEST/server.js
@@ -1,20 +1,20 @@
 var express = require('express');
 var app = express();
 var mongojs = require('mongojs');
-var db = mongojs('simplest',['productlist']);
+var db = mongojs('bbApp',['postlist']);
 var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
-app.get('/productlist', function(req, res){
+app.get('/postlist', function(req, res){
 	console.log("I received a GET request");
-	db.productlist.find(function(err,docs){
+	db.postlist.find(function(err,docs){
 		console.log(docs);
 		res.json(docs);
 	});
-app.post('/productlist', function(req, res){
+app.post('/postlist', function(req, res){
 	console.log(req.body);
-	db.productlist.insert(req.body,function(err,doc){
+	db.postlist.insert(req.body,function(err,doc){
 		res.json(doc);
 	});
 });
@@ -23,5 +23,5 @@ app.post('/productlist', function(req, res){
 });
 
 
-app.listen(3000);
-console.log("Server listening on port 3000");
+app.listen(8000);
+console.log("Server listening on port 8000");
